39793a00ea5eff5d0be3882f4d3611f28ff0d8ef
Ritwikmondal2004 <118586439+Ritwikmondal2004@users.noreply.github.com>
2025-04-13 10:55:08 +0000

    api

--- python_project/03.API/api.py
+++ python_project/03.API/api.py
@@ -8,4 +8,20 @@ def fetch_random_user_freeapi():
     data=response.json()
 
     if data["success"] and "data" in data:
-        
+        user_data = data["data"]
+        username=user_data["login"]["username"]
+        country=user_data["location"]["country"]
+        return username,country
+    else:
+        raise Exception("Failed to fetch data from API")
+    
+def main():
+    try:
+        username, country=fetch_random_user_freeapi()
+        print(f"Username: {username} \nCountry: {country}")
+    except Exception as e:
+        print(str(e))
+
+if __name__ == "__main__":
+    main()
+# if __name__ == "__main__":
